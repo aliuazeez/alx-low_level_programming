76df834b267d3af9efe4b221cf206bfb4891aa2f
aliuazeez <aliuazeez6m@gmail.com>
2022-09-09 16:37:30 +0100

    try

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - prints the last digit of the random
  * number stored in the variable n
- * return: - Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is a readme file exlaining what each program does
