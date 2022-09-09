fc2d791317d95c738a59c9ae6a54f7e1d5f1c668
aliuazeez <aliuazeez6m@gmail.com>
2022-09-09 19:04:46 +0100

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit
+ * using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
