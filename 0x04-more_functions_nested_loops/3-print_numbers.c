d820ff91be3181cc3fce3d7be8d4e041bdfd7b55
aliuazeez <aliuazeez6m@gmail.com>
2022-09-15 16:27:06 +0100

    gfgf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers,
+ * from 0 to 9
+ * followed by a new line
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	return (0);
+}
