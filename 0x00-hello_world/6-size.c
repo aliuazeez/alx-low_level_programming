2ddff34e329adaebca8a17d1c396ceec32b49df1
aliuazeez <aliuazeez6m@gmail.com>
2023-07-14 14:30:27 +0000

    task 6 printing types size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,17 @@
  */
 int main(void)
 {
-	printf("size of a char: %u byte(s)\n", (int)sizeof(char));
-	printf("size of an int: %u byte(s)\n", (int)sizeof(int));
-	printf("size of a long int: %u byte(s)\n", (int)sizeof(long int));
-	printf("size of a long long int: %u byte(s)\n", (int)sizeof(long long int));
-	printf("size of a float: %u byte(s)\n", (int)sizeof(float));
+	char a;
+	int i;
+	long int j;
+	long long int l;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", sizeof(a));
+	printf("size of an int: %lu byte(s)\n", sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", sizeof(j));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(l));
+	printf("size of a float: %lu byte(s)\n", sizeof(f));
+
 	return (0);
 }
