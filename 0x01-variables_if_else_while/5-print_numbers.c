0cb587e0a17b399648d8620707a1ee581749035c
aliuazeez <aliuazeez6m@gmail.com>
2023-07-16 16:59:33 +0000

    printing of numbers using putchar instead of printf

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * main - entry point
  *
- * a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * a program that prints all single digit numbers of base 10
  *
  * Return: always 0
  */
@@ -14,5 +14,5 @@ int main(void)
 		putchar(i + '0');
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
