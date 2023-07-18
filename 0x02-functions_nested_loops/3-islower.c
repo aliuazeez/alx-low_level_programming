0afdc49702c501b02b005a7282e0dd3a9c13d453
aliuazeez <aliuazeez6m@gmail.com>
2023-07-18 14:13:35 +0000

    task 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - xheck for for lowercase
- *
+ * @c: the character to b3 checked
  * Return: 1 if no error, else 0
  */
 int _islower(int c)
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if its alphabet
+ * @c: the value to b3 checked
+ * Return: 1 if no error else 0
+ */
+int _isalpha(int c)
+{
+	if ( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
