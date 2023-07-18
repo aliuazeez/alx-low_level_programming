5cc77432272661cbc8c30221310f521f16a4b268
aliuazeez <aliuazeez6m@gmail.com>
2023-07-18 16:05:48 +0000

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,9 +2,18 @@
 
 /**
  * print_last_digit - print last digit
+ * _abs - return absolute value
+ * @r: value input
  * @i: the value to check
  * Return: always 0
  */
+int _abs(int r)
+{
+	if (r < 0)
+		return (-r);
+	else
+		return (r);
+}
 int print_last_digit(int i)
 {
 	int b;
