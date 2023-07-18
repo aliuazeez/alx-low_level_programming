ed72d0268bb9a748fe430984d35b1bad1e2f8130
aliuazeez <aliuazeez6m@gmail.com>
2023-07-18 12:40:16 +0000

    second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_alphabet - a function that prints alphabets
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
