9b1e37b33ee65a87e6df78c4cf0fcfedc5f42d89
aliuazeez <aliuazeez6m@gmail.com>
2023-07-20 05:25:53 +0000

    g

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,8 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		_putchar(n + '0');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 		_putchar(',');
 		_putchar(' ');
 		
@@ -23,6 +24,8 @@ void print_to_98(int n)
 		}
 	}
 
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
 	_putchar(n + '0');
 	_putchar('\n');
 }
