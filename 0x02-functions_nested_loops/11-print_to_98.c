aa8c60a278a0f5e79bf9a94c79f5a8ecc0286159
aliuazeez <aliuazeez6m@gmail.com>
2023-07-20 04:59:39 +0000

    g

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print vwlue to 98
+ * @n: value to print to 98
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+		
+		if (n > 98)
+		{
+			n--;
+		}
+		else
+		{
+			n++;
+		}
+	}
+
+	_putchar(n + '0');
+	_putchar('\n');
+}
