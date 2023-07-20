a5666529d541eab0d39c8bbffbc624cb31d07554
aliuazeez <aliuazeez6m@gmail.com>
2023-07-20 03:55:50 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - sum two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: nothing
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,6 @@ int _abs(int r);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
-
+int add(int, int);
 
 #endif /* MAIN_H */
