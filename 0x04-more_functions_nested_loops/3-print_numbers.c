d953ebd914a67e46c4694b25d8e0728ee15b3703
aliuazeez <aliuazeez6m@gmail.com>
2023-07-21 23:06:20 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to p
+ * Return: numb3rs
+ */
+void print_numbers(void)
+{
+	int a;
+	
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
