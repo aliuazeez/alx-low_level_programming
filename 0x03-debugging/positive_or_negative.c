3bfd07dacf2042fff85d66430a34b0c5eaf5c707
aliuazeez <aliuazeez6m@gmail.com>
2023-07-21 07:43:58 +0000

    debugging task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * positive_or_negative - test for +ve or -ve
+ *
+ * Return: always 0
+ */
+/* betty style doc for function main goes there */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is zero\n", i);
+}
