9af3406161e8b02ad75655eb3486617732136357
aliuazeez <aliuazeez6m@gmail.com>
2023-07-22 10:08:13 +0000

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = 10; j <= 10; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i + '0');
-			_putchar('\n');
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
