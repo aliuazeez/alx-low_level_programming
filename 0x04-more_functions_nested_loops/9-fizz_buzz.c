a13baee38bb27fb990c4db507decf7e28afba03e
aliuazeez <aliuazeez6m@gmail.com>
2023-07-23 18:43:07 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,68 +1,28 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - starting point
- *
- * Return: always 0
+ * main - entry point
+ * print fizz for 3, buzs for 5 and fizzbuzz 
+ * for number divisible by 3 and 5
+ * Return: 0
  */
-void pfizz(void)
-{
-	_putchar('F');
-	_putchar('i');
-	_putchar('z');
-	_putchar('z');
-}
-void pbuzz(void)
-{
-	_putchar('B');
-	_putchar('u');
-	_putchar('z');
-	_putchar('z');
-}
-void pfb(void)
-{
-	_putchar('F');
-	_putchar('i');
-	_putchar('z');
-	_putchar('z');
-	_putchar('B');
-	_putchar('u');
-	_putchar('z');
-	_putchar('z');
-}
-
 int main(void)
 {
 	int i;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i > 9)
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar(i + '0');
-			_putchar(' ');
-		}
-		if ((i % 3) == 0)
-		{
-			pfizz();
-			_putchar(' ');
-		}
-		else if ((i % 5) == 0)
-		{
-			pbuzz();
-			_putchar(' ');
-		}
-		else if (((i % 3) && (i % 5)) == 0)
-		{
-			pfb();
-			_putchar(' ');
-		}
+			if (((i % 3) == 0) && ((i % 5) == 0))
+				printf("%s ", fb);
+			else if ((i % 3) == 0)
+				printf("%s ", fizz);
+			else if ((i % 5) == 0)
+				printf("%s ", buzz);
+			else
+				printf("%d ", i);
 	}
+	putchar('\n');
 	return (0);
 }
