4fb47533a180a5585cfa9231aa41b6597d4b96b4
aliuazeez <aliuazeez6m@gmail.com>
2023-07-25 20:06:16 +0000

    fist commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int strlen - vounts the no of string
+ * @s: string to br counted
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i++;
+	}
+	
+	*s = i;
+}
