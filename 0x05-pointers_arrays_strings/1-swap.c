6fc0b86909dd7328d1b92d5850ac88552a17f1c1
aliuazeez <aliuazeez6m@gmail.com>
2023-07-25 18:38:46 +0000

    fist commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap two int
+ * @a: int 1
+ * @b: int 2
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int *i;
+
+	*b = *i;
+	*a = *b;
+	*i = *a;
+}
