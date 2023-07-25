ec839d9e336e68b46010616a7e52aca35a286319
aliuazeez <aliuazeez6m@gmail.com>
2023-07-25 17:55:19 +0000

    fist commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a pointer that reset to 98
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	int *i;
+
+	i = n;
+
+	*i = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+this is a README file for the pointer task.
+But trust me I will update this later when I actually knoes what I am really doing
