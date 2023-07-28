c2482fbf5feb4348577b64567289ea6b65c3bf09
aliuazeez <aliuazeez6m@gmail.com>
2023-07-28 16:14:59 +0000

    commut

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - print string in reverse
+ * @s: string to print
+ * Return: void
+ */
+void rev_string(char *s);
+{
+	int i;
+	int l = 0;
+	
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (i = l - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
