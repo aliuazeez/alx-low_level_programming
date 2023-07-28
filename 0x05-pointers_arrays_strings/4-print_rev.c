53c4e9b6d1476098ef8d2544907a78f352bf025d
aliuazeez <aliuazeez6m@gmail.com>
2023-07-28 15:16:40 +0000

    commut

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,22 +5,23 @@
  * @s: string to print
  * Return: void
  */
-void print_rev(char *s) {
+void print_rev(char *s)
+{
     if (s == NULL)
         return;
 
-    // Calculate the length of the string
     int length = 0;
-    while (s[length] != '\0') {
+    while (s[length] != '\0')
+    {
         length++;
     }
 
-    // Print the string in reverse
-    for (int i = length - 1; i >= 0; i--) {
+    int i;
+
+    for (i = length - 1; i >= 0; i--)
+    {
         _putchar(s[i]);
     }
 
-    // Print a new line at the end
     _putchar('\n');
-}
 }
