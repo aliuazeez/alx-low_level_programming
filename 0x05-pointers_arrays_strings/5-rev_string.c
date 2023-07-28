ffa112b7e76182f1b5ad2e8b5e9db908d8fdba63
aliuazeez <aliuazeez6m@gmail.com>
2023-07-28 16:44:25 +0000

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,12 @@ void rev_string(char *s)
 {
 	int i;
 	int l = 0;
-	
+
 	while (s[l] != '\0')
-	{
 		l++;
-	}
 	for (i = l - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
+		_putchar(s[i];
 	}
+	_putchar('\n');
 }
