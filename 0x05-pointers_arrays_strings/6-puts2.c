50bb36d2ba69fc3664da5b6b306a71fd1b76e2d7
aliuazeez <aliuazeez6m@gmail.com>
2023-08-04 16:23:22 +0000

    comit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print a char and \n
+ * @str: string to print in 2
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int l = 0;
+
+	if (*str == '\0')
+		return;
+	while (*str != '\0')
+		l++;
+	for (i = 0; l <= 0; i++)
+	{
+		l--;
+		char t = str[i];
+
+		str[i] = str[i + 1];
+		str[i + 1] = t;
+	}
+
