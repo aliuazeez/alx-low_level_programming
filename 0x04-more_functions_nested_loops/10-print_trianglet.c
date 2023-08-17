645a434e26bd25e8f444f02af1b65ec3a7a13877
aliuazeez <aliuazeez6m@gmail.com>
2023-08-17 09:19:42 +0000

    commit

--- 0x04-more_functions_nested_loops/10-print_trianglet.c
+++ 0x04-more_functions_nested_loops/10-print_trianglet.c
@@ -1,29 +1,30 @@
 #include "main.h"
+
 /**
- * print_triangle - print triangle
- * @size: number of size of triangle
- * Return: void
+ * print_triangle - prints diagonal in n time
+ * @size: number of time n prints
+ * Return: nothing
  */
 void print_triangle(int size)
 {
 	int i, j;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
+
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j < size - i; j++)
 		{
 			_putchar(' ');
 		}
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < i + 1; j++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
-
