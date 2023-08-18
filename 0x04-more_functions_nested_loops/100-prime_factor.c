3725ea961a255f8460c575c6dbd05d35f21ce351
aliuazeez <aliuazeez6m@gmail.com>
2023-08-18 10:32:01 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main program
+ * Return: always 0
+ */
+int main(void)
+{
+	long int n = 612852475143;
+	int i = 2;
+	
+	while (n % i != 0 && n / i == 1)
+	{
+		i++;
+	}
+	printf("%d \n", i);
+	return (0);
+}
