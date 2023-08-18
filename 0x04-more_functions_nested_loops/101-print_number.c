275971dda66a4b1074bec5337f08554936a5fb10
aliuazeez <aliuazeez6m@gmail.com>
2023-08-18 09:12:59 +0000

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * print_number - prints number
  * @n: the number to print
@@ -6,23 +5,14 @@
  */
 void print_number(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 	{
-		if (n == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('-');
-			n = -n;
-			print_number(n / 10);
-			_putchar((n % 10) + '0');
-		}
+		_putchar('-');
+		n = -n;
 	}
-	else
+	if (n / 10 != 0)
 	{
 		print_number(n / 10);
-		_putchar((n % 10) + '0');
 	}
+	_putchar((n % 10) + '0');
 }
