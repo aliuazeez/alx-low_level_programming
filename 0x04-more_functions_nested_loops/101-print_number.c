2fa0ecac453899496ab3042279c09df4fc2d7d11
aliuazeez <aliuazeez6m@gmail.com>
2023-08-18 08:08:03 +0000

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,8 +6,15 @@
  */
 void print_number(int n)
 {
-	if (n <= 9)
+	if (n == 0)
 		_putchar(n + '0');
+	elseif (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n / 10);
+		_putchar((n % 10) + '0')
+	}
 	else
 	{
 		print_number(n / 10);
