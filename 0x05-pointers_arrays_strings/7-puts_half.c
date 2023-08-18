d6ddf23f71e23765ceb4824086dd33a963524fa4
aliuazeez <aliuazeez6m@gmail.com>
2023-08-18 13:14:19 +0000

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print 2nd half of a string
+ * @str: string to print
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+	int i = 0;
+	int j = 0;
+
+	if (*str == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	while (str[n] != '\0')
+		n++;
+	if (n % 2 != 0)
+		i = (n - 1) / 2;
+	for (j = (i / 2); j < i; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
