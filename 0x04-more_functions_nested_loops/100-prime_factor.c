5b43fc534a1e06c437ad47e06ba47861abf0abe7
aliuazeez <aliuazeez6m@gmail.com>
2023-08-18 10:54:00 +0000

    commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,14 +6,20 @@
 int main(void)
 {
 	long int n = 612852475143;
-	int i = 2;
-	int j;
+	long int i = 2;
+	long int j = 0;
 
-	while (n % i != 0 && n / i == 1)
+	while (n > 1)
 	{
+		if (n % i == 0)
+		{
+			j = i;
+			while (n / i == 0)
+			{
+				n /= i;
+			}
+		}
 		i++;
-		j = i;
 	}
-	printf("%d \n", j);
 	return (0);
 }
