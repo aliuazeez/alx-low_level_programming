c104d197b501002f37d7c78ea17cd52fa038c16f
aliuazeez <aliuazeez6m@gmail.com>
2023-08-19 01:07:53 +0000

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints array
+ * @a: a pointer
+ * @n: the size of array to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+	int j = n;
+
+	if (n == 0)
+	{
+		putchar('\n');
+		return;
+	}
+	i = n;
+	while (i != 0)
+	{
+		n -= i;
+		i--;
+		printf("%d", a[n]);
+		n = j;
+		if (i != 0)
+			printf(", ");
+		else
+			putchar('\n');
+	}
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * _strcpy - copy strings
+ * @dest: string destination
+ * @src: string source to be copied
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp = '\0';
+	int n = 0;
+	int i = n;
+	int j = 0;
+
+	if (*src == '\0')
+	{
+		putchar('\n');
+	}
+	while (src[n] != '\0')
+		n++;
+	i = n + 1;
+	temp = src;
+
+	while (j != i)
+	{
+		dest[j] = temp[j];
+		j++;
+	}
+	return (dest);
+}
