0296983080f106f702e56f9424670cbed9faa53c
aliuazeez <aliuazeez6m@gmail.com>
2023-10-08 23:56:03 +0000

    hshhssj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-#define QUOTE "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 /**
  * main - main application
  * Return: 1
  */
 int main(void)
 {
-	write(2, QUOTE, sizeof(QUOTE));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
