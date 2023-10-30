1a87cfc12111e5092aeb023db44d67bd212bcb6f
aliuazeez <aliuazeez6m@gmail.com>
2023-10-30 07:35:42 +0000

    gfg

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - prints x to the power of y
+ * @x: base
+ * @y: exponent
+ * Return: pow(x, y)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (x != 0 && y > 0)
+		return (x * _pow_recursion(x, y - 1))
+}
