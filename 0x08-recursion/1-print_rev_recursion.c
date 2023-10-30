9d8480f8e812fa4c962f0baf4e5b4827719228c4
aliuazeez <aliuazeez6m@gmail.com>
2023-10-30 00:15:43 +0000

    hhh

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: string to be printed
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	if (i > 0)
+	{
+		putchar(s[i - 1]);
+		_print_rev_recursion(s);
+		i--;
+	}
+	else
+		return;
+
