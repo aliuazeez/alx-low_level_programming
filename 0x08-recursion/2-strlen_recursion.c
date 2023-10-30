100f562fea2580e29712f6e8130d688ef1609a72
aliuazeez <aliuazeez6m@gmail.com>
2023-10-30 00:48:03 +0000

    hhh

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print string length
+ * @s: string to check
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		i++;
+		return (i);
+	}
+	else
+		return (i);
+}
