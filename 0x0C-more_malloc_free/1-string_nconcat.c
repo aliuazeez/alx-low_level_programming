64965a03980fa278a8455150319c2d80001dd744
aliuazeez <aliuazeez6m@gmail.com>
2023-11-08 11:15:18 +0000

    that

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 	while (s1[s1_len] != '\0')
 		s1_len++;
 	while (s2[s2_len] != '\0')
